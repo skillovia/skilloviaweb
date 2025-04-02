7257f1551ba508c5368b4cae7193b2f3872f12ed
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-04-02 18:39:43 +0100

    london updated

--- src/pages/auth/signup/Phone.jsx
+++ src/pages/auth/signup/Phone.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Facebook, Apple } from "lucide-react";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom"; // Add useNavigate
